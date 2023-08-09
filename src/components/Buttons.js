11e5ffa86d82188bc7f0f54b425323e962c3e385
Zach <amidstbooks@gmail.com>
2023-08-08 23:30:18 -0500

    Added steps from Task 4: Update Buttons.js. Page is displaying properly.

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,28 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+import satData from "./satData.js"
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
+  // const filterByType = props.filterByType;
+  // const setSat = props.setSat;
+  // const displaySats = props.displaySats;
+  return(
+      <div>
+      {displaySats.map((sat, id) => {
+        return (
+          <div>
+            <button onClick={() => filterByType(sat)} key={id}>
+              {sat} Orbit
+            </button>
+    
+          </div>
+        );
+      })}
+    
+        <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
-  );
+  )
+ 
+
+
 };
 
 export default Buttons;
